98cfc7186ee2fbd397a3af210742152674bb2d16
Han Qi <hanqi11991@hotmail.com>
2021-08-07 23:44:15 +0800

    activate actions for heroku but without secrets

--- bbquote/lib.py
+++ bbquote/lib.py
@@ -4,10 +4,11 @@ import requests
 
 def get_quote():
     # url = 'https://movie-quote-api.herokuapp.com/v1/quote/'  # alternative API
-    url = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes'
+    url = "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
     response = requests.get(url).json()[0]
 
-    return response['quote'],response['author']
+    return response["quote"], response["author"]
+
 
 if __name__ == "__main__":
     print(get_quote())
