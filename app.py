4193981e63d60758d9da9c4e5238b6e542c36760
Han Qi <hanqi11991@hotmail.com>
2021-08-07 18:47:20 +0800

    moved app.py out 1 level

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from bbquote.quote import get_quote
+from bbquote.lib import get_quote
 
 author, quote = get_quote()  # assuming the function returns an author and a quote
 
